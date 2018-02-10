593200696627509886cdcb1d1cef12672758f88f
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-02-10 16:36:52 +0100

    Add Cart and CartDetail entities to Domain Project

---
+++ ComputerShop/ComputerShop.Domain/Entites/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop.Domain.Entites
+{
+    public class Cart
+    {
+        #region Columns
+        [Key]
+        public uint ID { get; set; }
+
+        [ForeignKey("User")]
+        public uint UserID { get; set; }
+        public virtual User User { get; set; }
+        #endregion
+
+        public virtual List<CartDetail> CartDetails { get; set; }
+    }
+}
---
+++ ComputerShop/ComputerShop.Domain/Entites/CartDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop.Domain.Entites
+{
+    public class CartDetail
+    {
+        #region Columns
+        [Key]
+        public uint ID { get; set; }
+
+        [ForeignKey("Cart")]
+        public uint CartID { get; set; }
+        public virtual Cart Cart { get; set; }
+
+        [ForeignKey("Product")]
+        public uint ProductID { get; set; }
+        public virtual Product Product { get; set; }
+
+        public uint Quantity { get; set; }
+        #endregion
+    }
+}
