a01ccd6d58ce25458090921df53a99782fec44b2
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-02-10 13:55:57 +0100

    Add NinjectWebCommon and some test classes for checking if Ninject works

---
+++ ComputerShop/ComputerShop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComputerShop.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDoStuff Do;
+        // GET: Home
+
+        public HomeController()
+        {
+
+        }
+
+        public HomeController(IDoStuff stuff)
+        {
+            this.Do = stuff;
+        }
+
+        public string Index()
+        {
+            return Do.Property;
+        }
+    }
+}
---
+++ ComputerShop/ComputerShop.MVC/Controllers/IDoStuff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerShop.MVC.Controllers
+{
+    public interface IDoStuff
+    {
+        string Property { get; set; }
+    }
+}
