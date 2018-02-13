c759f594b776560aa4e2561e2200940a061720c4
DESKTOP-NJF0PAT\jakub <jakubkuniszewski1@gmail.com>
2018-02-13 20:54:08 +0100

    Add additional DataAnnotations to Entities, and update csproj file

--- ComputerShop/ComputerShop.Domain/Entites/Category.cs
+++ ComputerShop/ComputerShop.Domain/Entites/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace ComputerShop.Domain.Entites
     {
         #region Columns
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         [Required,MaxLength(30),MinLength(3)]
         public string Name { get; set; }
